183c7c4241318dc258d352e5b820fdf475e7b79b
Tayla Coman <tayla@developersquared.co.nz>
2016-08-22 13:23:19 +1200

    first commit - added room views

---
+++ app/Http/Controllers/AngularController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AngularController extends Controller
+{
+    public function serveApp()
+    {
+        return view('index');
+    }
+
+    public function unsupported()
+    {
+        return view('unsupported_browser');
+    }
+
+    public function rooms()
+    {
+        return view('rooms');
+    }
+}
